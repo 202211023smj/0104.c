a5f7d31ee0ee70ac74f6020a3c5f559f3a1d3b69
202211023smj <121844264+202211023smj@users.noreply.github.com>
2023-01-06 09:09:05 +0900

    Add files via upload

---
+++ Arduino.ino 2days___.ino
@@ -0,0 +1,28 @@
+// C++ code
+//
+void setup()
+{
+ int i;
+ 
+  for(i=2;i<10;i++)
+  {
+   pinMode(i, OUTPUT); 
+  }  
+}
+
+void loop()
+{
+  int i;
+  
+  for(i=2;i<10;i++)
+  {
+    digitalWrite(i, HIGH);
+  delay(500); // Wait for 1000 millisecond(s)
+  }
+  
+  for(i=9;i>=2;i--)
+  {
+    digitalWrite(i, LOW);
+  delay(500); // Wait for 1000 millisecond(s)
+  }
+}
